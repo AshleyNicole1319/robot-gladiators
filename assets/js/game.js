feff847b52fde2505a7a23c26b46e8371eb3d9ae
AshleyNicole1319 <a-warford96@comcast.net>
2022-06-22 12:38:33 -0400

    Practicing the JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("This is an alert! JavaScript is running");
+// This creates a function named "fight" // AKA Declares the funciton
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//This allows player to input information because of .prompt // var gives the user input somewhere to be stored
+var playerName = window.prompt("What is your robot's name?");
+
+//Notice the lack of quotation marks
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message")
+
+console.log(10+10)
+
+console.log("Our robot's name is " + playerName)
+
+var name = "your name";
+console.log(name);
+// This calls the function we previously declared
+//fight ()
