32e15d2a34dc2b0819d8a4c1306b0d7715de8584
AshleyNicole1319 <a-warford96@comcast.net>
2022-06-28 10:27:53 -0400

    fix JS formatting

--- assets/js/game.js
+++ assets/js/game.js
@@ -42,13 +42,14 @@ var randomNumber = function(min, max) {
   
   // fight function (now with parameter for enemy's object holding name, health, and attack values)
   var fight = function(enemy) {
+    
     // keep track of who goes first
     var isPlayerTurn = true;
   
-    // randomly change turn order
-    if (Math.random() > 0.5) {
-      isPlayerTurn = false;
-    }
+        // randomly change turn order
+        if (Math.random() > 0.5) {
+         isPlayerTurn = false;
+        }
   
     while (playerInfo.health > 0 && enemy.health > 0) {
       if (isPlayerTurn) {
